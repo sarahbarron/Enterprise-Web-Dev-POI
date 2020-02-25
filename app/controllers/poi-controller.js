adca4424490ec70bc5c5ab73f87b0f77debfadd1
sarahbarron <sarahflavin@yahoo.com>
2020-02-25 21:26:12 +0000

    login and signup views setup and routes to the views

--- app/controllers/poi-controller.js
+++ app/controllers/poi-controller.js
@@ -1,7 +1,17 @@
 const Poi = {
     index: {
-        handler: function(request, h){
-            return h.file('./app/views/main.html')
+        handler: function(request, h) {
+            return h.file('./app/views/main.html');
+        }
+    },
+    signup: {
+        handler: function(request, h) {
+            return h.file('./app/views/signup.html');
+        }
+    },
+    login: {
+        handler: function(request, h) {
+            return h.file('./app/views/login.html');
         }
     }
 };
--- routes.js
+++ routes.js
@@ -2,12 +2,20 @@ const Poi = require('./app/controllers/poi-controller')
 
 
 module.exports = [
+    // signup route
+    { method: 'GET', path: '/signup', config: Poi.signup },
+    // login route
+    { method: 'GET', path: '/login', config: Poi.login },
+    // Route to home page
     { method: 'GET', path: '/', config: Poi.index },
+
+
     {
         method: 'GET',
         path: '/{param*}',
         handler: {
             directory: {
+                // set the path for the public folder
                 path: './public'
             }
         }
