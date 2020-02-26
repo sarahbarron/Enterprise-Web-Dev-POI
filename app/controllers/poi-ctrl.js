c6e965b81bcded7d4c912f3ad2a5e82a71d88565
sarahbarron <sarahflavin@yahoo.com>
2020-02-26 11:58:09 +0000

    Information from the add poi form is being saved and presented as a list on the allpois.hbs page

--- app/controllers/poi-ctrl.js
+++ app/controllers/poi-ctrl.js
@@ -6,17 +6,22 @@ const Poi = {
     },
     allpois: {
         handler: function(request, h) {
-            return h.view('allpois', { title: 'All created POIs' });
+            return h.view('allpois',
+                {
+                    title: 'All created POIs',
+                    poi: this.poi
+                });
         }
     },
 
     addpoi:{
         handler: function(request, h) {
             const data = request.payload;
+            let userEmail = request.auth.credentials.id
+            data.user = this.users[userEmail]
             this.poi.push(data);
             return h.redirect('/allpois');
-
-    }
+        }},
 };
 
 module.exports = Poi;
