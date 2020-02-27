ffcda7042a40711cb107cd2852f52cfa7c2a8f5f
sarahbarron <sarahflavin@yahoo.com>
2020-02-27 14:19:15 +0000

    setup to get environment variables from heroku instead of local env file

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 // const dotenv = require('dotenv')
 // if the .env file cant be found handle the error
 // const result = dotenv.config();
-
 // if (result.error) {
 //     console.log(result.error.message);
 //     process.exit(1);
 // }
+//
 
 const Hapi = require('@hapi/hapi');
 
